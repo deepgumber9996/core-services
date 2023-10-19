57ac48eb6a14493f1a5aa5d47af2278221e43a4b
ABHISHEK  RANA <121286986+abhirana04@users.noreply.github.com>
2023-10-19 13:31:03 +0530

    Update Organization.java

--- egov-pb-digilocker/src/main/java/org/egov/dx/web/models/Organization.java
+++ egov-pb-digilocker/src/main/java/org/egov/dx/web/models/Organization.java
@@ -3,6 +3,7 @@ package org.egov.dx.web.models;
 import javax.xml.bind.annotation.XmlAttribute;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,35 +12,77 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@SuppressWarnings("unused")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @EqualsAndHashCode
-@XStreamAlias("Organization")
-
+@XStreamAlias("organization")
 public class Organization {
-	
-	@XmlAttribute
-    @XStreamAlias("name")
+    @XStreamAsAttribute
     private String name;
+    @XStreamAsAttribute
+    private String code;
+    @XStreamAsAttribute
+    private String tin;
+    @XStreamAsAttribute
+    private String type;
+    @XStreamAsAttribute
+    private String uuid;
     
-    @XmlAttribute
-    @XStreamAlias("code")
-    private String code="";
-    
-    @XmlAttribute
-    @XStreamAlias("tin")
-    private String tin="";
-    
-    @XmlAttribute
-    @XStreamAlias("uid")
-    private String uid;
-    
-    @XmlAttribute
-    @XStreamAlias("type")
-    private String type="SG";
-	
-    @XStreamAlias("Address")
     private Address address;
+
+    // Constructors, getters, and setters
+    // ...
+
+    // Getter and setter methods for attributes
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTin() {
+        return tin;
+    }
+
+    public void setTin(String tin) {
+        this.tin = tin;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    
+    public String getuuid() {
+        return uuid;
+    }
+
+    public void setuuid(String uuid) {
+        this.uuid = uuid;
+    }
 }
+
